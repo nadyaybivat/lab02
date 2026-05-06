c8bae8865991648965a30ae22330b7cc01868484
Надюшка <metelskaya23@yandex.ru>
2026-05-06 09:56:57 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,7 +5,7 @@
 int main() {
     std::string name;  // переменная для имени пользователя
     std::cout << "Enter your name: ";  // запрос имени
-    std::cin >> name;  // чтение имени из ввода
+    std::cin >> name;  // чтение имени
     std::cout << "Hello world from " << name << std::endl;  // приветствие
     return 0;  // успешное завершение
 }
